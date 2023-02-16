07fe596e9e786482682106e4d0d1207ac7c7711d
kwambzval <valentinevkkv@gmail.com>
2023-02-16 07:14:10 -0800

    size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Sizes
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int g;
+	float h;
+
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(g));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(h));
+return (0);
+}
