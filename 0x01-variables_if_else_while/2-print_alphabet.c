6edaf315cf7363e9460c1c19418821e268a5d7a2
kwambzval <valentinevkkv@gmail.com>
2023-02-17 07:46:21 -0800

    lowercase alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main -ALPHABETS IN lowercase
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while
+		(c <= 'z') {
+			putchar(c);
+			c++;
+		}
+	putchar('\n');
+	return (0);
+}
