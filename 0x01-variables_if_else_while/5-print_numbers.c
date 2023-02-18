4076b7f09c4d0ece4acbdee9df0daadd9d0df725
kwambzval <valentinevkkv@gmail.com>
2023-02-18 05:55:54 -0800

    base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main -0 to 10
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	putchar('\n');
+	return (0);
+}
