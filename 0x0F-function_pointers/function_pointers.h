e675f2e13c630f67fb30ee3fbf01c67f6d945912
kwambzval <valentinevkkv@gmail.com>
2023-03-22 15:05:47 -0700

    task 0

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -5,7 +5,9 @@
 #include <stdlib.h>
 
 void print_name(char *name, void (*f)(char *));
+int _putchar(char c);
 void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
+
 #endif
+
