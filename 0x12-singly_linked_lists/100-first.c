66f6de7e92ed6974f0e116769f9a7a556dbe8d87
kwambzval <valentinevkkv@gmail.com>
2023-05-01 00:55:53 -0700

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * pre_main_message - Prints a message before main function execution
+ *
+ * Description: This function prints a message to the standard output stream
+ *              before the main function is executed.
+ * Return:      void
+ */
+
+void pre_main_message(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+			"I bore my house upon my back!\n");
+}
