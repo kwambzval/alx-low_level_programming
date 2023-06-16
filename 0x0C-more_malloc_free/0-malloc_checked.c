0f2e294c2ce7410fac67bc646dd0a1bb6b4e89c9
kwambzval <valentinevkkv@gmail.com>
2023-06-16 06:07:49 -0700

    Task 0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * malloc_checked - malloc memory allocation.
+ * @b: Number of bytes to alloacate.
+ *
+ * Return: Pointer to the allocated memory else 98 if fails.
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
+}
