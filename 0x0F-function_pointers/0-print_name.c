74782d56e79519ad2bee4f0f5d2f4bb222d4c75f
kwambzval <valentinevkkv@gmail.com>
2023-06-22 23:51:09 -0700

    task 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a string
+ * @name: The name to be printed
+ * @f: a function that takes the name as its argument and return onthing
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
+
