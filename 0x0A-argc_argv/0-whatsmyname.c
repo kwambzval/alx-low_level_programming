5281f915494f55484f2fd0191a8ee6074d96d1ce
kwambzval <valentinevkkv@gmail.com>
2023-06-26 06:48:43 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * @argc: the number of arguments in a command line
+ * @argv: array of arguments in a command line
+ *
+ * Return: 0 Sucess
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,13 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+void print_program_name(char *argv[]);
+void print_argument_count(int argc);
+void print_all_arguments(int argc, char *argv[]);
+int multiply_numbers(int num1, int num2);
+int add_positive_numbers(int argc, char *argv[]);
+int calculate_minimum_coins(int cents);
+
+#endif
+
