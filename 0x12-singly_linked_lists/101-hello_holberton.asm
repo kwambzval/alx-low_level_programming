bca7fcd93bb9e21fdb6653bb65324e8158127c23
kwambzval <valentinevkkv@gmail.com>
2023-07-02 05:53:29 -0700

    task 6

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,26 +1,16 @@
 section .data
-    message db 'Hello, Holberton', 0
-    format db '%s', 10, 0
+    format db "Hello, Holberton", 0
 
 section .text
-    global main
+    extern printf
 
+global main
 main:
-    ; Set up stack frame
-    push rbp
-    mov rbp, rsp
-
-    ; Call printf
-    mov rdi, format
-    mov rsi, message
-    mov rax, 0
+    sub rsp, 8
+    mov edi, format
+    xor eax, eax
     call printf
-
-    ; Clean up stack frame
-    mov rsp, rbp
-    pop rbp
-
-    ; Exit
-    mov rax, 0
+    add rsp, 8
+    xor eax, eax
     ret
 
