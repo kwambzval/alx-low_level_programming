7fbf0c934c84442d20ce29be3bf4e8862b4ded7a
kwambzval <valentinevkkv@gmail.com>
2023-07-03 04:29:12 -0700

    task 0

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,24 +1,19 @@
-#include "main.h"
-#include <limits.h>
-#include <stdio.h>
 #include <stdlib.h>
+#include "main.h"
 
 /**
- * malloc_checked - memory allocation using malloc
- * @b: Number of bytes to be alloacate
+ * malloc_checked - Allocates memory using malloc
+ * @b: Number of bytes to allocate
  *
- * Return: Pointer to the allocated memory or 98 if fails.
+ * Return: Pointer to the allocated memory
+ *         Exit with status value of 98 if malloc fails
  */
-
 void *malloc_checked(unsigned int b)
 {
 	void *ptr = malloc(b);
 
 	if (ptr == NULL)
-	{
-	       exit(98);
-	}
-
+		exit(98);
 	return (ptr);
 }
 
--- 0x0C-more_malloc_free/README.md
+++ 0x0C-more_malloc_free/README.md
@@ -53,3 +53,4 @@ If the number of arguments is incorrect, print Error, followed by a new line, an
 num1 and num2 should only be composed of digits. If not, print Error, followed by a new line, and exit with a status of 98
 You are allowed to use more than 5 functions in your file
 You can use bc (man bc) to check your results.
+
