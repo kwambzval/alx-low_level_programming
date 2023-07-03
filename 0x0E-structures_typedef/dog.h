9733333c3d7bc2a506d0869e851e04e925db80df
kwambzval <valentinevkkv@gmail.com>
2023-07-03 06:36:05 -0700

    task 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
 
-typedef struct dog {
-    char *name;
-    float age;
-    char *owner;
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
 } Dog;
-
 #endif
 
