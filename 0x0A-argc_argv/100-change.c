6f575a80e03e45a6858829d8eb91f458a32feb73
kwambzval <valentinevkkv@gmail.com>
2023-07-05 02:46:08 -0700

    task 5

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -1,40 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int calculate_minimum_coins(int cents);
+
 /**
  * main - Entry point
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
+ * @argc: Number of command line arguments
+ * @argv: Array of command line arguments
+ *
  * Return: 0 on success, 1 on error
  */
 int main(int argc, char *argv[])
 {
-	int cents, numCoins, count, i;
-	int coins[] = {25, 10, 5, 2, 1};
+	int cents, coins = 0;
 
-	if (argc != 2)
+	if (argc != 2 || argv[1] == NULL)
 	{
-		printf("Usage: %s <cents>\n", argv[0]);
+		printf("Error\n");
 		return (1);
 	}
 
 	cents = atoi(argv[1]);
-
 	if (cents < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	numCoins = sizeof(coins) / sizeof(coins[0]);
-	count = 0;
 
-	for (i = 0; i < numCoins; i++)
+	coins = calculate_minimum_coins(cents);
+	printf("%d\n", coins);
+
+	return (0);
+}
+
+/**
+ * calculate_minimum_coins - Calculate the minimum number of coins required
+ * @cents: Amount of cents
+ *
+ * Return: Minimum number of coins
+ */
+int calculate_minimum_coins(int cents)
+{
+	int coins = 0;
+
+	while (cents > 0)
 	{
-		count += cents / coins[i];
-		cents %= coins[i];
+		if (cents >= 25)
+			cents -= 25;
+		else if (cents >= 10)
+			cents -= 10;
+		else if (cents >= 5)
+			cents -= 5;
+		else if (cents >= 2)
+			cents -= 2;
+		else
+			cents -= 1;
+		coins++;
 	}
 
-	printf("%d\n", count);
-	return (0);
+	return (coins);
 }
 
