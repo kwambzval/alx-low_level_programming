d43eb7aa21cbc1d6e971247dc589f36630f4aa3c
kwambzval <valentinevkkv@gmail.com>
2023-09-04 10:17:01 -0700

    task 4

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -5,58 +5,15 @@
 #include <elf.h>
 #include "main.h"
 #include <string.h>
+
 /**
  * error_exit - Exits the program with an error message.
  * @message: The error message to print.
  */
 void error_exit(const char *message)
 {
-    dprintf(STDERR_FILENO, "%s\n", message);
-    exit(98);
-}
-
-/**
- * print_magic - Prints the ELF magic number.
- * @header: Pointer to the ELF header.
- */
-void print_magic(Elf64_Ehdr *header)
-{
-    int i;
-
-    printf("  %-20s:", "Magic");
-    for (i = 0; i < EI_NIDENT; i++)
-        printf(" %02x", header->e_ident[i]);
-    printf("\n");
-}
-
-/**
- * print_elf_class - Prints the ELF class (32-bit or 64-bit).
- * @header: Pointer to the ELF header.
- */
-void print_elf_class(Elf64_Ehdr *header)
-{
-    printf("  %-20s: %s\n", "Class",
-           (header->e_ident[EI_CLASS] == ELFCLASS32) ? "ELF32" : "ELF64");
-}
-
-/**
- * print_elf_data - Prints the ELF data encoding (little or big endian).
- * @header: Pointer to the ELF header.
- */
-void print_elf_data(Elf64_Ehdr *header)
-{
-    printf("  %-20s: %s\n", "Data",
-           (header->e_ident[EI_DATA] == ELFDATA2LSB)
-           ? "2's complement, little endian" : "2's complement, big endian");
-}
-
-/**
- * print_elf_version - Prints the ELF version.
- * @header: Pointer to the ELF header.
- */
-void print_elf_version(Elf64_Ehdr *header)
-{
-    printf("  %-20s: %d (current)\n", "Version", header->e_ident[EI_VERSION]);
+	dprintf(STDERR_FILENO, "%s\n", message);
+	exit(98);
 }
 
 /**
@@ -67,38 +24,39 @@ void print_elf_version(Elf64_Ehdr *header)
  */
 int main(int argc, char *argv[])
 {
-    int fd;
-    Elf64_Ehdr elf_header;
-    if (argc != 2)
-    {
-        error_exit("Usage: elf_header elf_filename");
-    }
+	int fd;
+	Elf64_Ehdr elf_header;
 
-    fd = open(argv[1], O_RDONLY);
+	if (argc != 2)
+	{
+		error_exit("Usage: elf_header elf_filename");
+	}
 
-    if (fd == -1)
-    {
-        error_exit("Error: Cannot open file");
-    }
+	fd = open(argv[1], O_RDONLY);
 
-    if (read(fd, &elf_header, sizeof(elf_header)) != sizeof(elf_header))
-    {
-        error_exit("Error: Cannot read ELF header");
-    }
+	if (fd == -1)
+	{
+		error_exit("Error: Cannot open file");
+	}
 
-    if (memcmp(elf_header.e_ident, ELFMAG, SELFMAG) != 0)
-    {
-        error_exit("Error: Not an ELF file");
-    }
+	if (read(fd, &elf_header, sizeof(elf_header)) != sizeof(elf_header))
+	{
+		error_exit("Error: Cannot read ELF header");
+	}
 
-    printf("ELF Header:\n");
+	if (memcmp(elf_header.e_ident, ELFMAG, SELFMAG) != 0)
+	{
+		error_exit("Error: Not an ELF file");
+	}
 
-    print_magic(&elf_header);
-    print_elf_class(&elf_header);
-    print_elf_data(&elf_header);
-    print_elf_version(&elf_header);
+	printf("ELF Header:\n");
 
-    close(fd);
-    return (0);
+	print_magic(&elf_header);
+	print_elf_class(&elf_header);
+	print_elf_data(&elf_header);
+	print_elf_version(&elf_header);
+
+	close(fd);
+	return (0);
 }
 
