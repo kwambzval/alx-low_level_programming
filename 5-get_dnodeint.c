f7f8b0c57a50a4fcb098516ee344694de6c919ee
kwambzval <valentinevkkv@gmail.com>
2024-01-11 14:08:18 -0800

    task 4-5

---
+++ 5-get_dnodeint.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - Returns the nth node of a dlistint_t linked list.
+ * @head: Pointer to the head of the list.
+ * @index: The index of the node, starting from 0.
+ *
+ * Return: The nth node of a dlistint_t
+ * linked list, or NULL if it doesn't exist.
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int count = 0;
+
+	while (head)
+	{
+		if (count == index)
+		{
+			return (head);
+		}
+		count++;
+		head = head->next;
+	}
+
+	return (NULL);
+}
+
