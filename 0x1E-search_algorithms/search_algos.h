c22cf956e7b0a8c13bb7b61673889f3f46850a57
kwambzval <valentinevkkv@gmail.com>
2024-05-21 08:28:56 -0700

    task 12

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,7 +3,7 @@
 
 #include <stdio.h>
 #include <math.h>
-
+#include <stdlib.h>
 /* Function Prototypes */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
